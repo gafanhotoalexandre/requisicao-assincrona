0e2d13378179945a1d90addcc2f9b4a4555a5eb2
Alexandre <alexandrevmartinsdelima@gmail.com>
2022-03-28 19:41:56 -0300

    criando estrutura de requisicao assincrona

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+const mainContainer = document.querySelector('.container');
+const app = mainContainer.querySelector('#active');
+let url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+async function getData() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+}
+
+async function mountStructure() {
+    const elements = {
+        div: document.createElement('div'),
+        h1: document.createElement('h1'),
+        p: document.createElement('p'),
+    }
+
+    const res = await getData();
+
+    fillStructure(elements, res);
+}
+
+function fillStructure({div, h1, p}, res) {
+    h1.innerText = res.title;
+    p.innerText = res.body;
+    div.appendChild(h1);
+    div.appendChild(p);
+
+    document.querySelector('.container').appendChild(div);
+}
+
+app.addEventListener('click', (event) => mountStructure());
+    // fetch(url)
+    //     .then(response => response.json())
+    //     .then(res => mountStructure(res));
+// });
